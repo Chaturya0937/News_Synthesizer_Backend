f16c1784e9ff2997d3d133831a9050c965e88849
yadalasrihari186 <yadalasrihari186@gmail.com>
2026-05-15 15:18:46 +0530

    Update NewsApi.py
    rti

--- Agents/NewsApi.py
+++ Agents/NewsApi.py
@@ -1,10 +1,16 @@
 import requests
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 Api_key = os.getenv("NEWSAPI")
 
 def get_news_from_NewsApi(Topic):
-    response=requests.get(f'https://newsapi.org/v2/everything?q={Topic}&apiKey={Api_key}&pagesize=10')
-    data=response.json()
-    return data["articles"]
+    # Using python f-string correctly to inject the generated query topic
+    url = f'https://newsapi.org/v2/everything?q={Topic}&apiKey={Api_key}&pageSize=10'
+    response = requests.get(url)
+    data = response.json()
+    
+    if "articles" in data:
+        return data["articles"]
+    return []
